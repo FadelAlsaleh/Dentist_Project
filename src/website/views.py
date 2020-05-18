63b0fa8108a094647b62fb4d78aac1a3b369c976
FadelAlsaleh <53988195+FadelAlsaleh@users.noreply.github.com>
2020-05-18 14:06:33 +0300

    secound commit for this project

--- src/website/views.py
+++ src/website/views.py
@@ -32,3 +32,15 @@ def contact(request):
 
     else:
         return render (request , 'contact.html' , {})
+
+
+
+
+def about(request):
+    return render (request , 'about.html' , {})
+
+def pricing(request):
+    return render (request , 'pricing.html' , {})
+
+def service(request):
+    return render (request , 'service.html' , {})        
